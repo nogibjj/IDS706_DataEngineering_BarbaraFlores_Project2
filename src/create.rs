1db26877e56539835baed09c3bc4b3d309139ff6
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-10-30 06:55:49 +0000

    update

---
+++ src/create.rs
@@ -0,0 +1,12 @@
+extern crate rusqlite;
+extern crate prettytable;
+
+use rusqlite::Connection;
+use prettytable::{Table, Row, Cell};
+use std::time::{Instant};
+
+// TEST
+pub fn my_library_function() {
+    println!("¡Hola desde la biblioteca!");
+}
+
